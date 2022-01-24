5a4466fc148c9722d8e3310b5a0994ed53ebe1d1
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-01-24 12:18:10 -0300

    fixed tests

--- tests/test_analysys/test_epistats.py
+++ tests/test_analysys/test_epistats.py
@@ -11,5 +11,5 @@ from epigraphhub.analysis.epistats import prevalence
 def test_prevalence(a, b):
     p = prevalence(1000, 300, a, b)
     assert isinstance(p, st._distn_infrastructure.rv_frozen)
-    assert (p.mean(), 0.3)
+    assert p.mean() == pytest.approx(0.3, 0.1)
     assert p.std() > 0
