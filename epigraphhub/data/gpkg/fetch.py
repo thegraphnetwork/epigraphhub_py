52a8415bec890b6de15aff401032b1dffeb8f84f
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-21 21:26:07 -0400

    chore(structure): Move python code from epigraphhub

---
+++ epigraphhub/data/gpkg/fetch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, run
+import shlex
+import os
+import inquirer
+from glob import glob
+
+URL = "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/"
+
+questions = [
+    inquirer.Path('maps_dir',
+                  message="Enter directory where the gpkg should be downloaded to (don't forget the trailing '/')",
+                  path_type=inquirer.Path.DIRECTORY,
+                  ),
+    inquirer.Confirm('clean', message='Do you want to delete the zipped after GPKGs have been extracted?')
+]
+
+
+def get_codes():
+    codes = []
+    with open('ISO-alpha3-codes.csv', 'r', encoding='utf8') as f:
+        for l in f.readlines():
+            codes.append(l.split(';')[0])
+    return codes
+
+
+def fetch_files(savepath, clean=False):
+    for code in get_codes():
+        proc = Popen(shlex.split(f'wget {URL}gadm36_{code}_gpkg.zip'))
+        proc.wait()
+    for zf in glob('*.zip'):
+        run(f"unzip '{zf}'")
+    if clean:
+        os.unlink('*.zip')
+
+
+if __name__ == "__main__":
+    answers = inquirer.prompt(questions)
+    fetch_files(answers['maps_dir'], answers['clean'])
---
+++ epigraphhub/data/gpkg/load.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import geopandas as gpd
+import inquirer
+import glob
+import os
+import json
+from sqlalchemy import create_engine
+import fiona
+
+from epigraphhub import config
+
+
+questions = [
+    inquirer.Path('maps_dir',
+                  message="Enter directory where the gpkg maps located(don't forget the trailing '/')",
+                  path_type=inquirer.Path.DIRECTORY,
+                  )
+]
+
+
+def process_maps(gdf):
+    """
+    Adds additional geometry columns to the map
+    :param gdf: geodataframe
+    """
+    # create JSON column
+    gdf['json'] = [json.dumps(pol.__geo_interface__) for pol in gdf.geometry]
+    gdf['Latitude'] = [pol.centroid.y for pol in gdf.geometry]
+    gdf['Longitude'] = [pol.centroid.x for pol in gdf.geometry]
+    return gdf
+
+
+def insert_into_postgis(pth):
+    maps = glob.glob(os.path.join(pth, '*gadm36_*.gpkg'))
+    engine = create_engine(config.DB_URI)
+    for m in maps:
+        fname = os.path.split(m)[-1]
+        fname_noext = fname.split('.')[0]
+        layers = fiona.listlayers(m)
+        print(f"Inserting {len(layers)} layers of {fname} into PostGIS.")
+        for i, layer in enumerate(layers):  # Importing all layers separately
+            Map = gpd.read_file(m, layer=f'{layer}', driver='GPKG')
+            Map = process_maps(Map)
+            country_name = fname_noext.split('_')[1]
+            try:
+                Map.to_postgis(f"{country_name}_{i}", engine, if_exists='replace')
+            except Exception as e:
+                print(f"Loading of {country_name}_{i} map failed:\n{e}")
+
+
+
+def main(answers):
+    insert_into_postgis(answers['maps_dir'])
+
+
+if __name__ == '__main__':
+    answers = inquirer.prompt(questions)
+    main(answers)
