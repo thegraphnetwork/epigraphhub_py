11fd0cba1dcc69ac9168f7a2179732ab51e82244
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-03-30 20:48:28 +0200

    Added class to introspect the WorldPOP REST API, and download any dataset from it

---
+++ epigraphhub/data/worldpop.py
@@ -0,0 +1,42 @@
+import requests as rq
+import json
+import pandas as pd
+
+
+def json_get(url):
+    response = rq.get(url)
+    return response.json()
+
+
+class WorldPop:
+    api_root = 'https://www.worldpop.org/rest/data'
+
+    def __init__(self):
+        self._datasets = None
+        self.aliases = [d['alias'] for d in self.datasets['data']]
+
+    @property
+    def datasets(self):
+        if self._datasets is None:
+            self._datasets = json_get(self.api_root)
+        return self._datasets
+
+    def __str__(self):
+        df = pd.DataFrame(self.datasets['data'])
+        return df.to_markdown()
+
+    def get_dataset_tables(self, alias):
+        """
+        Generator function that returns dictionarys containing on dataframe and metadata at a time
+        Args:
+            alias: alias for the dataset of interest
+        """
+        if alias not in self.aliases:
+            raise NameError(f"'{alias}' is invalid dataset alias")
+
+        alias_url = self.api_root + f'/{alias}'
+        content = json_get(alias_url)
+        for ds in content['data']:
+            table = json_get(alias_url + f"/{ds['alias']}")['data']
+            df = pd.DataFrame(table, )
+            yield {'alias': ds['alias'], 'name': ds['name'], 'data': df}
---
+++ tests/test_data/test_worldpop.py
@@ -0,0 +1,27 @@
+import pytest
+from epigraphhub.data.worldpop import WorldPop
+import pandas as pd
+
+
+def test_list_datasets():
+    WP = WorldPop()
+    ds = WP.datasets
+    assert isinstance(ds, dict)
+    assert len(WP.aliases) > 0
+
+
+def test_print():
+    WP = WorldPop()
+    p = WP.__str__()
+    assert p.startswith('|')
+    assert 'alias' in p
+    assert 'title' in p
+
+
+def test_get_ds_tables():
+    WP = WorldPop()
+    for df in WP.get_dataset_tables('pop'):
+        assert isinstance(df, dict)
+        assert isinstance(df['data'], pd.DataFrame)
+        assert 'name' in df
+        assert len(df['data']) > 0
