8d07a2054dd65612c1942a0348ba621b0b062a54
Eduardo Correa Araujo <eduardocorreaaraujo8@gmail.com>
2022-03-30 21:51:26 -0300

    corret bug in ngboost models

--- epigraphhub/models/ngboost_models.py
+++ epigraphhub/models/ngboost_models.py
@@ -35,9 +35,9 @@ params_model = {
     "natural_gradient": True,
     "verbose": False,
     "col_sample": 0.9,
-    "n_estimators": 300,
+    "n_estimators": 30,
     "learning_rate": 0.1,
-    "validation_fraction": 0.25,
+    "validation_fraction": 0.15,
     "early_stopping_rounds": 50,
 }
 
@@ -168,7 +168,7 @@ def rolling_predictions(
             y95 = y95[: len(y95) + dif]
 
         df_pred = pd.DataFrame()
-        df_pred["target"] = target[1:]
+        df_pred["target"] = target
         df_pred["date"] = x
         df_pred["lower"] = y5
         df_pred["median"] = y50
@@ -184,7 +184,7 @@ def rolling_predictions(
         x = np.array(x)
 
         df_pred = pd.DataFrame()
-        df_pred["target"] = target[1:]
+        df_pred["target"] = target
         df_pred["date"] = x
         df_pred["lower"] = [0.0] * len(df_pred)
         df_pred["median"] = [0.0] * len(df_pred)
