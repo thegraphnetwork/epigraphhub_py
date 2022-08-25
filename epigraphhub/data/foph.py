d81add52cf4d970a48c91d8905a8f4fa73d90875
Eduardo Correa Araujo <eduardocorreaaraujo8@gmail.com>
2022-08-25 02:22:09 -0300

    Refactoring ngboost_models and add code to generate the dashboard results

--- epigraphhub/data/foph.py
+++ epigraphhub/data/foph.py
@@ -23,9 +23,6 @@ from sqlalchemy import create_engine
 engine_public = create_engine(
     "postgresql://epigraph:epigraph@localhost:5432/epigraphhub"
 )
-engine_private = create_engine(
-    "postgresql://epigraph:epigraph@localhost:5432/privatehub"
-)
 
 
 def get_agg_data(schema, table_name, columns, method, ini_date):
@@ -36,8 +33,7 @@ def get_agg_data(schema, table_name, columns, method, ini_date):
     (e.g. regions name), and the third the column that will be used to compute the
     result of the aggregation.
 
-    :params schema: string. The country that you want to get the data, for now, the
-                    only options are: ['switzerland', 'colombia'].
+    :params schema: string. The schema where the data that you want to get is saved.
 
     :params table_name: string. Name of the table that you want to get the data.
 
@@ -58,11 +54,6 @@ def get_agg_data(schema, table_name, columns, method, ini_date):
     table_name = table_name.lower()
     method = method.upper()
 
-    accepted_countrys = ["switzerland", "colombia"]
-
-    if schema not in accepted_countrys:
-        raise Exception(f"Error. The only countries accepted are: {accepted_countrys}.")
-
     query = f"SELECT {columns[0]}, {columns[1]}, {method}({columns[2]}) FROM {schema}.{table_name} WHERE {columns[0]} > '{ini_date}' GROUP BY ({columns[0]}, {columns[1]})"
 
     df = pd.read_sql(query, engine_public)
@@ -72,13 +63,12 @@ def get_agg_data(schema, table_name, columns, method, ini_date):
     return df
 
 
-def get_georegion_data(schema, table_name, georegion, columns):
+def get_georegion_data(schema, table_name, georegion, columns, georegion_column):
     """
     This function provides a data frame for the table selected in the param table_name and
     the chosen regions in the param georegion.
 
-    ":params schema: string. The country that you want to get the data, for now, the only options are:
-                            ['switzerland', 'colombia'].
+    :params schema: string. The schema where the data that you want to get is saved.
 
     :params table_name: string. Name of the table that you want to get the data.
 
@@ -87,40 +77,13 @@ def get_georegion_data(schema, table_name, georegion, columns):
 
     :params columns: list of strings| None. Columns that you want to select from the table table_name. If None all the columns will be returned.
 
+    :params georegion_column: string. Name of the column to filter by georegion name.
+
     :return: Dataframe
     """
 
     schema = schema.lower()
     table_name = table_name.lower()
-
-    accepted_countrys = ["switzerland"]
-
-    accepted_tables = {
-        "switzerland": [
-            "foph_cases",
-            "foph_casesVaccPersons",
-            "foph_covidCertificates",
-            "foph_death",
-            "foph_deathVaccPersons",
-            "foph_hosp",
-            "foph_hospcapacity",
-            "foph_hospVaccPersons",
-            "foph_intCases",
-            "foph_re",
-            "foph_test",
-            "foph_testPcrAntigen",
-            "foph_virusVariantsWgs",
-        ],
-        "colombia": ["casos_positivos_covid"],
-    }
-
-    if schema not in accepted_countrys:
-        raise Exception(f"Error. The only countries accepted are: {accepted_countrys}.")
-
-    if table_name not in accepted_tables[schema]:
-        raise Exception(
-            f"Error. The only curves accepted are: {accepted_tables[schema]}."
-        )
 
     if type(georegion) != list and georegion != "All":
         raise Exception(
@@ -134,25 +97,27 @@ def get_georegion_data(schema, table_name, georegion, columns):
         )
 
     if columns == None:
-        columns = "*"
+        s_columns = "*"
 
-    # separe the columns by comma to apply in the sql query
-    s_columns = ""
-    for i in columns:
+    else:
+        # separe the columns by comma to apply in the sql query
+        s_columns = ""
+        for i in columns:
 
-        s_columns = s_columns + i + ","
+            s_columns = s_columns + i + ","
 
-    s_columns = s_columns[:-1]
+        s_columns = s_columns[:-1]
 
     if georegion == "All":
         query = f"select {s_columns} from {schema}.{table_name}"
 
     if len(georegion) == 1:
-        query = f"select {s_columns}  from {schema}.{table_name} where \"{columns[1][1:-1]}\" = '{georegion[0]}' ;"
+
+        query = f"select {s_columns}  from {schema}.{table_name} where {georegion_column} = '{georegion[0]}' ;"
 
     if len(georegion) > 1 and type(georegion) == list:
         georegion_tuple = tuple(i for i in georegion)
-        query = f'select {s_columns} from {schema}.{table_name} where "{columns[1][1:-1]}" in {georegion_tuple} ;'
+        query = f'select {s_columns} from {schema}.{table_name} where "{georegion_column}" in {georegion_tuple} ;'
 
     df = pd.read_sql(query, engine_public)
 
@@ -160,33 +125,40 @@ def get_georegion_data(schema, table_name, georegion, columns):
 
 
 dict_cols = {
-    "foph_cases": ["datum", '"geoRegion"', "entries"],
-    "foph_test": ["datum", '"geoRegion"', "entries", "entries_pos"],
-    "foph_hosp": ["datum", '"geoRegion"', "entries"],
-    "foph_hospcapacity": [
+    "foph_cases_d": ["datum", "georegion", "entries"],
+    "foph_test_d": ["datum", "georegion", "entries", "entries_pos"],
+    "foph_hosp_d": ["datum", "georegion", "entries"],
+    "foph_hospcapacity_d": [
         "date",
-        '"geoRegion"',
-        '"ICU_Covid19Patients"',
-        '"Total_Covid19Patients"',
+        "georegion",
+        "icu_covid19patients",
+        "total_covid19patients",
     ],
-    "foph_re": ["date", "geoRegion", "median_R_mean"],
+    "foph_re_d": ["date", "georegion", "median_r_mean"],
+}
+
+georegion_columns = {
+    "foph_cases_d": "georegion",
+    "foph_test_d": "georegion",
+    "foph_hosp_d": "georegion",
+    "foph_hospcapacity_d": "georegion",
 }
 
 date_columns = {
-    "foph_cases": "datum",
-    "foph_test": "datum",
-    "foph_hosp": "datum",
-    "foph_hospcapacity": "date",
+    "foph_cases_d": "datum",
+    "foph_test_d": "datum",
+    "foph_hosp_d": "datum",
+    "foph_hospcapacity_d": "date",
 }
 
 count_columns = {
-    "foph_cases": ["entries"],
-    "foph_test": ["entries"],
-    "foph_hosp": ["entries"],
-    "foph_hospcapacity": ["ICU_Covid19Patients", "Total_Covid19Patients"],
+    "foph_cases_d": ["entries"],
+    "foph_test_d": ["entries"],
+    "foph_hosp_d": ["entries"],
+    "foph_hospcapacity_d": ["icu_covid19patients", "total_covid19patients"],
 }
 
-columns_name = {"foph_cases": "cases", "foph_test": "test", "foph_hosp": "hosp"}
+columns_name = {"foph_cases_d": "cases", "foph_test_d": "test", "foph_hosp_d": "hosp"}
 
 
 def get_cluster_data(
@@ -195,6 +167,7 @@ def get_cluster_data(
     georegion,
     dict_cols=dict_cols,
     date_columns=date_columns,
+    georegion_columns=georegion_columns,
     count_columns=count_columns,
     columns_name=columns_name,
     vaccine=True,
@@ -205,35 +178,34 @@ def get_cluster_data(
     This function provides a data frame where each column is associated with a table
     and region selected.
 
-    :params schema: string. The country that you want to get the data, for now, the only \
-                    options are: ['switzerland', 'colombia']
+    :params schema: string. The schema where the data that you want to get is saved.
 
-    :params table_name: list of strings. In this list should be all the tables that you 
-                        want get the data. 
+    :params table_name: list of strings. In this list should be all the tables that you
+                        want get the data.
 
-    :params georegion: list of strings. This list contains all the regions of the country 
+    :params georegion: list of strings. This list contains all the regions of the country
                         of interest or the string 'All' to return all the regions.
 
-    :params dict_cols: dictionary. In the keys are the table_names and in the values 
+    :params dict_cols: dictionary. In the keys are the table_names and in the values
                       the columns that you want to use from each table
-    
+
     :params date_columns:dictionary. In the keys are the table_names and in the values
-                          the name of the date column of the table to be used as the 
-                          index. 
+                          the name of the date column of the table to be used as the
+                          index.
 
     :params count_columns: dictionary. In the keys are the table_names and in the values
-                        the name of the column which values will be used. 
-    
+                        the name of the column which values will be used.
+
     :params columns_name: dictionary. In the keys ate the table_names and in the values
-                        the name that will appear in the column associated with each 
-                    table in the final data frame that will be returned. 
-        
+                        the name that will appear in the column associated with each
+                    table in the final data frame that will be returned.
+
     :params vaccine: boolean. If True the data of total vaccinations per hundred for the
-                            country in the schema will be added in the final data frame. 
-                            This data is from our world in data. 
-                            
-    :params smooth: boolean. If True in the end data frame will be applied a moving 
-                            average of seven days. 
+                            country in the schema will be added in the final data frame.
+                            This data is from our world in data.
+
+    :params smooth: boolean. If True in the end data frame will be applied a moving
+                            average of seven days.
 
     :return: Dataframe
     """
@@ -242,21 +214,27 @@ def get_cluster_data(
 
     for table in table_name:
 
-        df = get_georegion_data(schema, table, georegion, dict_cols[table])
+        df = get_georegion_data(
+            schema,
+            table,
+            georegion,
+            dict_cols[table],
+            georegion_column=georegion_columns[table],
+        )
         df.set_index(date_columns[table], inplace=True)
         df.index = pd.to_datetime(df.index)
 
-        for region in df.geoRegion.unique():
+        for region in df.georegion.unique():
 
             for count in count_columns[table]:
 
-                if table == "foph_hospcapacity":
+                if table == "foph_hospcapacity_d":
 
                     names = {
-                        "ICU_Covid19Patients": "ICU_patients",
-                        "Total_Covid19Patients": "total_hosp",
+                        "icu_covid19patients": "icu_patients",
+                        "total_covid19patients": "total_hosp",
                     }
-                    df_aux1 = df.loc[df.geoRegion == region].resample("D").mean()
+                    df_aux1 = df.loc[df.georegion == region].resample("D").mean()
 
                     df_aux2 = pd.DataFrame()
 
@@ -267,7 +245,7 @@ def get_cluster_data(
                     df_end = pd.concat([df_end, df_aux2], axis=1)
 
                 else:
-                    df_aux1 = df.loc[df.geoRegion == region].resample("D").mean()
+                    df_aux1 = df.loc[df.georegion == region].resample("D").mean()
 
                     df_aux2 = pd.DataFrame()
 
@@ -313,34 +291,3 @@ def get_cluster_data(
         df_end = df_end.dropna()
 
     return df_end
-
-
-def get_updated_data_swiss(smooth=True):
-
-    """
-    Function to get the updated data for Geneva
-
-    :params smooth: Boolean. If True, a rolling average is applied
-
-    :return: Dataframe.
-    """
-
-    df = pd.read_sql_table(
-        "hug_hosp_data",
-        engine_private,
-        schema="switzerland",
-        columns=["Date_Entry", "Patient_id"],
-    )
-
-    df.index = pd.to_datetime(df.Date_Entry)
-    df_hosp = df.resample("D").count()
-    df_hosp = df_hosp[["Patient_id"]]
-
-    if smooth == True:
-        df_hosp = df_hosp[["Patient_id"]].rolling(window=7).mean()
-        df_hosp = df_hosp.dropna()
-
-    df_hosp = df_hosp.sort_index()
-    df_hosp.rename(columns={"Patient_id": "hosp_GE"}, inplace=True)
-
-    return df_hosp.loc[df_hosp.index >= "2021-09-01"]
