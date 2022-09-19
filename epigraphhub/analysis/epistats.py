52b6ceb1ccaa9940eafe2e718194fbf2060f4c0e
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-09-19 12:32:37 -0300

    Added function to calculate Incidence rate

--- epigraphhub/analysis/epistats.py
+++ epigraphhub/analysis/epistats.py
@@ -1,9 +1,10 @@
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 import numpy as np
 import pandas as pd
 import scipy.stats as st
 
 
-def prevalence(pop_size: int, positives: int, a: float = 1, b: float = 1) -> object:
+def posterior_prevalence(pop_size: int, positives: int, a: float = 1, b: float = 1) -> st.rv_continuous:
     """
     Returns the Bayesian posterior prevalence of a disease for a point in time.
     It assumes number of cases follow a binomial distribution with probability described as a beta(a,b) distribution
@@ -21,3 +22,28 @@ def prevalence(pop_size: int, positives: int, a: float = 1, b: float = 1) -> obj
     pa = a + positives
     pb = b + pop_size - positives
     return st.beta(pa, pb)
+
+
+@np.vectorize
+def incidence_rate(pop_size: int, new_cases: int, scaling: float = 1e5) -> Union[float, np.ndarray, np.ndarray]:
+    """
+    incidence is defined as the number of new cases in a population over a period of time, typically 1 year. The incidence rate is also usually scale to 100k people to facilitate comparisons between localities withe different populations.
+    Parameters
+    ----------
+    pop_size: population pop_size
+    new_cases: number of new cases observed in the period
+    scaling: number to scale the rate to. If ommitted, the rate is return as cases per 100k.
+
+    Returns
+    -------
+    A float or a np.ndarray of floats
+
+    Examples
+    --------
+    >>> incidence_rate(1000, 5)
+    500.0
+    >>> incidence_rate([1000,5000,10000], [5,5,5])
+    array([500, 100, 50])
+    """
+    IR = new_cases / pop_size * scaling
+    return IR
--- tests/test_analysis/test_epistats.py
+++ tests/test_analysis/test_epistats.py
@@ -3,13 +3,25 @@ Tests for epistats module
 """
 import pytest
 import scipy.stats as st
+import numpy as np
+from numpy.testing import assert_equal
 
-from epigraphhub.analysis.epistats import prevalence
+from epigraphhub.analysis.epistats import posterior_prevalence, incidence_rate
 
 
 @pytest.mark.parametrize(("a", "b"), [(1, 1), (2, 2)])
 def test_prevalence(a, b):
-    p = prevalence(1000, 300, a, b)
+    p = posterior_prevalence(1000, 300, a, b)
     assert isinstance(p, st._distn_infrastructure.rv_frozen)
     assert p.mean() == pytest.approx(0.3, 0.1)
     assert p.std() > 0
+
+
+@pytest.mark.parametrize(("pop", "cases", "expected"),
+                         [(1000, 5, 500),
+                          ([1000, 5000, 10000], [5, 5, 5], np.array([500, 100, 50]))]
+                         )
+def test_incidence_rate(pop, cases, expected):
+    ir = incidence_rate(pop, cases)
+    assert isinstance(ir, np.ndarray)
+    assert_equal(ir, expected)
