52d20ed17e58c8f1389a84f05134a63e8df50452
Luã Bida Vacaro <luabidaa@gmail.com>
2022-12-05 10:54:11 -0300

    Aggravate to disease

--- epigraphhub/data/brasil/sinan/extract.py
+++ epigraphhub/data/brasil/sinan/extract.py
@@ -7,23 +7,23 @@ from epigraphhub.data._config import SINAN_LOG_PATH
 
 logger.add(SINAN_LOG_PATH, retention="7 days")
 
-aggravates = SINAN.agravos
+diseases = SINAN.agravos
 
 
-def download(aggravate: str):
+def download(disease: str):
     """
-    Download all parquets available for an aggravate,
+    Download all parquets available for an disease,
     according to `SINAN.agravos`.
 
     Attrs:
-        aggravate (str): The aggravate to be downloaded.
+        disease (str): The disease to be downloaded.
         data_dir (str) : The output directory were files will be downloaded.
-                         A directory with the aggravate code will be created.
+                         A directory with the disease code will be created.
 
     Returns:
         parquets_paths_list list(PosixPath) : A list with all parquets dirs.
     """
     
-    SINAN.download_all_years_in_chunks(aggravate)
+    SINAN.download_all_years_in_chunks(disease)
     
-    logger.info(f"All years for {aggravate} downloaded at /tmp/pysus")
+    logger.info(f"All years for {disease} downloaded at /tmp/pysus")
