e90588c720115edfd85b514923cbcb558b1533e7
Luã Bida Vacaro <82233055+luabida@users.noreply.github.com>
2023-01-16 17:31:57 -0300

    fix(pypath): add init to brasil.sinan data collection (#207)
    * bugfix(pythonpath): add __init__ to brasil.sinan data collection
    
    * Changing /var/log to /tmp
    
    * Add dunder init on brasil dir as well

--- epigraphhub/data/_config.py
+++ epigraphhub/data/_config.py
@@ -7,18 +7,18 @@ commonly used in data collection modules
 # Colombia COVID data config:
 from sodapy import Socrata
 
-COLOMBIA_LOG_PATH = "/var/log/colombia_fetch.log"
+COLOMBIA_LOG_PATH = "/tmp/colombia_fetch.log"
 COLOMBIA_CLIENT = Socrata("www.datos.gov.co", "078u4PCGpnDfH157kAkVFoWea")
 
 
 # Federal Office of Public Health (FOPH) COVID data config:
-FOPH_LOG_PATH = "/var/log/foph_fetch.log"
+FOPH_LOG_PATH = "/tmp/foph_fetch.log"
 FOPH_URL = "https://www.covid19.admin.ch/api/data/context"
 FOPH_CSV_PATH = "/tmp/foph/releases"
 
 
 # Our World in Data (OWID) COVID data config:
-OWID_LOG_PATH = "/var/log/owid_fetch.log"
+OWID_LOG_PATH = "/tmp/owid_fetch.log"
 OWID_CSV_URL = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 OWID_CSV_PATH = "/tmp/owid/releases"
 OWID_FILENAME = OWID_CSV_URL.split("/")[-1]
@@ -26,4 +26,4 @@ OWID_HOST = "135.181.41.20"
 
 
 # SINAN data config:
-SINAN_LOG_PATH = "/var/log/sinan_fetch.log"
+SINAN_LOG_PATH = "/tmp/sinan_fetch.log"
