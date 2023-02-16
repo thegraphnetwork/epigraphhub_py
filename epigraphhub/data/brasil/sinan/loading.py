2f84a9925e5f0f3f5de07247f73f21a962dd0978
Luã Bida Vacaro <luabidaa@gmail.com>
2023-02-16 14:18:23 -0300

    Fix \x00 error on SIFG

--- epigraphhub/data/brasil/sinan/loading.py
+++ epigraphhub/data/brasil/sinan/loading.py
@@ -1,18 +1,18 @@
 import os
-from pathlib import Path
+import pandas as pd
 
+from pathlib import Path
 from loguru import logger
 from pangres import upsert
-from pysus.online_data import parquets_to_dataframe as to_df
 
+from epigraphhub.settings import env
 from epigraphhub.connection import get_engine
 from epigraphhub.data._config import SINAN_LOG_PATH
-from epigraphhub.settings import env
 
 from . import DISEASES, normalize_str
 
-logger.add(SINAN_LOG_PATH, retention="7 days")
 
+logger.add(SINAN_LOG_PATH, retention="7 days")
 engine = get_engine(credential_name=env.db.default_credential)
 
 
@@ -38,13 +38,12 @@ def upload(parquet_dirs: list):
     for dir in parquet_dirs:
         if "parquet" in Path(dir).suffix and any(os.listdir(dir)):
 
-            df = to_df(str(dir), clean_after_read=False)
-
-            df.columns = df.columns.str.lower()
-            df.index.name = "index"
+            df = _read_parquets_dir(dir)
             disease_code = str(dir).split("/")[-1].split(".parquet")[0][:-4]
             tablename = "sinan_" + normalize_str(di_codes[disease_code]) + "_m"
             schema = "brasil"
+
+            print(f'Inserting {dir} on {schema}.{tablename}')
 
             with engine.connect() as conn:
                 try:
@@ -59,8 +58,35 @@ def upload(parquet_dirs: list):
                         create_table=True,
                     )
 
-                    logger.info(f"Table {tablename} updated")
+                    print(f"Table {tablename} updated")
 
                 except Exception as e:
                     logger.error(f"Not able to upsert {tablename} \n{e}")
                     raise e
+                        
+
+def _read_parquets_dir(path: str) -> pd.DataFrame:
+    chunks = Path(path).glob('*.parquet')
+    chunks_dfs = list()
+
+    try:
+        for parquet in chunks:
+            df = pd.read_parquet(
+                str(parquet),
+                engine='fastparquet'
+            )
+            objs = df.select_dtypes(object)
+            df[objs.columns] = objs.apply(
+                lambda x: x.str.replace('\x00', '')
+            )
+            chunks_dfs.append(df)
+
+        final_df = pd.concat(chunks_dfs, ignore_index=True)
+        final_df.columns = df.columns.str.lower()
+        final_df.index.name = "index"
+
+        return final_df
+        
+    except Exception as e:
+        logger.error(e)
+        raise e
