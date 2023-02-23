fafe06e1b251b6a478da048eae01f79db6e61844
Luã Bida Vacaro <luabidaa@gmail.com>
2023-02-23 15:15:36 -0300

    Fixing metadata files

--- epigraphhub/data/brasil/sinan/extract.py
+++ epigraphhub/data/brasil/sinan/extract.py
@@ -1,12 +1,18 @@
+import os
+import pandas as pd
+
+from pathlib import Path
 from loguru import logger
+
 from pysus.online_data import SINAN
 
+from epigraphhub.data.brasil.sinan import DISEASES
 from epigraphhub.data._config import PYSUS_DATA_PATH, SINAN_LOG_PATH
 
 logger.add(SINAN_LOG_PATH, retention="7 days")
 
 
-def download(disease: str):
+def download(disease: str) -> None:
     """
     Download all parquets available for a disease,
     according to `SINAN.agravos`.
@@ -18,3 +24,32 @@ def download(disease: str):
     SINAN.download_all_years_in_chunks(disease, data_dir=PYSUS_DATA_PATH)
 
     logger.info(f"All years for {disease} downloaded at {PYSUS_DATA_PATH}")
+
+
+def metadata_df(disease: str) -> pd.DataFrame:
+    """ 
+    Reads metadata sheets located at `./metadata` as DataFrames.
+    """
+    code = DISEASES[disease]
+    metadata_file = f'{code}.xlsx'
+    metadata_dir = Path(__file__).parent / 'metadata'
+    
+    metadata_avail = [
+        code_m[:4]
+        for code_m 
+        in os.listdir(metadata_dir) 
+        if code_m.endswith('.xlsx')
+    ]
+    
+    if code in metadata_avail:
+        df =  pd.read_excel(metadata_dir / metadata_file)
+        df.to_csv(metadata_dir / f'{code}.csv', )
+
+    else:
+        logger.error(f'Metadata not available for {disease}')
+
+
+if __name__ == "__main__":
+    for d in DISEASES:
+
+        metadata_df(d)
